43118c7aac92006e874f245d8ad97048c3adddd5
root <root@localhost.localdomain>
2016-11-20 08:04:20 -1000

    rm link maker

--- make-links.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make links for backup and restore:
-# Giving them different names to the same basic program
-
-ln -srf mybackup myrestore
-
-ln -srf mybackup mybackup.media
-ln -srf mybackup myrestore.media
